2c01864304de1470bf42f60c06b8d7fb0d64f2f6
bhavesh patel <learn3184@gmail.com>
2020-12-17 15:45:25 +0000

    Initial commit

---
+++ src/main/java/guru/springframework/controller/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage()
+    {
+        System.out.println("something again and again ");
+        return "Index";
+
+    }
+}
